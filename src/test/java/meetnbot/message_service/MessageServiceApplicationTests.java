23e636fffb0cb38d8a5eb3c18d68c21dbdc2ef50
Anton <Agressive.cat@yandex.ru>
2023-12-03 21:53:50 +0300

    init

---
+++ src/test/java/meetnbot/message_service/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package meetnbot.message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
