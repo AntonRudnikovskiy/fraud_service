9628167e2c4e075cbe5de73126efe9e0a4cecc23
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-12 00:00:54 +0300

    Transactional

--- src/main/java/sentinelguard/fraud_service/service/FraudCheckService.java
+++ src/main/java/sentinelguard/fraud_service/service/FraudCheckService.java
@@ -2,6 +2,7 @@ package sentinelguard.fraud_service.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sentinelguard.fraud_service.entity.FraudCheckHistory;
 import sentinelguard.fraud_service.repository.FraudCheckHistoryRepository;
 
@@ -11,6 +12,8 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class FraudCheckService {
     private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    @Transactional
     public boolean isFraudulentCustomer(Integer customerId) {
         fraudCheckHistoryRepository.save(
                 FraudCheckHistory.builder()
